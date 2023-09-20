72ed2feaa16a8a4f3b3fd11b97844003d530399c
Sailesh Ghimire <saileshghimire.sg@gmail.com>
2023-09-20 22:28:11 +0545

    update

--- src/context/ProjectList.js
+++ src/context/ProjectList.js
@@ -1,5 +1,7 @@
-import project from '../assets/project.png'
-import textform from '../assets/text.jpg'
+import ecomm from '../assets/ecom.jpg'
+import textform from '../assets/textform.jpg'
+import sql from '../assets/bootstrap.jpg'
+
 
 export const ProjectList = [
     {
@@ -14,14 +16,14 @@ export const ProjectList = [
     {
         id: 2,
         name: "ECommerce App",
-        image: project,
+        image: ecomm,
         github: "https://github.com/Sailesh-ghimire/E-Commerce-SIte",
         live: ""
     },
     {
         id: 3,
         name: "PHP based WebPage",
-        image: project,
+        image: sql,
         github: "https://github.com/Sailesh-ghimire/website-using-bootstrap",
         live: ""
     },
